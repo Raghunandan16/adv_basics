087ac86a55a10940b706ca3755069ce6468f0cb0
Raghunandan <16raghunandangupta@gmail.com>
2023-12-30 23:51:07 +0530

    added quiz file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,22 +9,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
+    return const Quiz();
   }
 }

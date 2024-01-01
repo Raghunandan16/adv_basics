f26bd79a0a6e0cb29ea3d1f8c01ae04ae49964eb
Raghunandan <16raghunandangupta@gmail.com>
2024-01-01 21:51:41 +0530

    adding sample questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,13 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs? ',
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+];

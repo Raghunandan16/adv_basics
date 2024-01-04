6214cba58f04a4faff4231205a8b083c43bf173d
Raghunandan <16raghunandangupta@gmail.com>
2024-01-05 03:35:09 +0530

    Fixed Sized box and container pattern

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -24,10 +24,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
     final currentQuestion = questions[currentQuestionIndex];
-    return Container(
-      margin: const EdgeInsets.all(40),
-      child: SizedBox(
-        width: double.infinity,
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,

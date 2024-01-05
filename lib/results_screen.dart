68d619bd84449bc6e1682040c521f2fa73ec5cbb
Raghunandan <16raghunandangupta@gmail.com>
2024-01-06 02:16:37 +0530

    added summary method using map in results_screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,8 +1,22 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswers});
   final List<String> chosenAnswers;
+  List<Map<String, Object>> getSummary() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].question,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summary;
+  }
+
   @override
   Widget build(context) {
     return SizedBox(
